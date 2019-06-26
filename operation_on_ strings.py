9601462b556e45c21cb74acf7852b3677c7eb0dd
rahulgiri2019 <rahulgiri201486@gmail.com>
2019-06-26 23:37:45 +0530

    add one file operation on strings

---
+++ operation_on_ strings.py
@@ -0,0 +1,10 @@
+# s= 'jaigiri'
+# print(len(s))
+
+# s = input("Enter main strings:")
+# subs = input("Enter sub strings:")
+# if subs in s:
+#     print(subs,'is found in main strings')
+# else:
+#     print(subs,'is not found in main strings')
+
