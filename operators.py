b55ce7874777c3756e9652a0765198be46de7c5d
rahulgiri2019 <rahulgiri201486@gmail.com>
2019-06-26 13:30:55 +0530

    add output statements

--- operators.py
+++ operators.py
@@ -127,6 +127,7 @@
 # from sys import argv
 # print(argv[1])
 
-from sys import argv
-print(argv[1]+argv[2])
-print(int(argv[1])+int(argv[2]))
+# from sys import argv
+# print(argv[1]+argv[2])
+# print(int(argv[1])+int(argv[2]))
+
---
+++ output statments.py
@@ -0,0 +1,14 @@
+# output statements
+# s='durga'
+# a=48
+# s1='java'
+# s2='python'
+# print('hello',s,'your age is :',a)
+# print('you are teaching ',s1, 'and',s2)
+
+# formatted string
+a=10
+b=20
+c=30
+print('a value is %i' %a)
+print(('b value is %d and c value is %d' %(b,c)))
