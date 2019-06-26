df4184b9620272dd79cd0384381031ea26d993f1
rahulgiri2019 <rahulgiri201486@gmail.com>
2019-06-26 13:44:00 +0530

    add flow control

--- output statments.py
+++ output statments.py
@@ -7,8 +7,24 @@
 # print('you are teaching ',s1, 'and',s2)
 
 # formatted string
-a=10
-b=20
-c=30
-print('a value is %i' %a)
-print(('b value is %d and c value is %d' %(b,c)))
+# a=10
+# b=20
+# c=30
+# print('a value is %i' %a)
+# print(('b value is %d and c value is %d' %(b,c)))
+
+# s= 'durga'
+# list=[10,20,30]
+# print('hello %s...The list of items are %s' %(s,list))
+
+name = 'durga'
+salary = 1000
+gf = 'sunny'
+print('hello {0} your salary is {1} and your friends {2} is waiting'.format(name,salary,gf))
+print('hello {x} your salary is {y} and your friends {z} is waiting'.format(x=name,y=salary,z=gf))
+
+
+
+
+
+
