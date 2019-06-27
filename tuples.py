2dc8ace080c349fef3accf6c166c9ba974409aa8
rahulgiri2019 <rahulgiri201486@gmail.com>
2019-06-27 17:49:35 +0530

    add tuples

---
+++ tuples.py
@@ -0,0 +1,50 @@
+# t = 10,20,30,40
+# print(t)
+# print(type(t))
+#
+# list = [10,20,30]
+# t = tuple(list)
+# print(t)
+
+# t= (10,20,30,40,50)
+# print(t[0])
+# print(t[-1])
+# print(t[100])
+#
+# t = (10,20,30,40)
+# t[1]=70
+# print(t)
+
+# t= (10,20,30,40,50,10,20,10,10)
+# print(len(t))
+# print(t.count(10))
+# print(t.index(20))
+
+# t=(40,10,30,20)
+# t1=sorted(t)
+# print(t1)
+# print(type(t))
+# print(t)
+# print(min(t))
+# print(max(t))
+
+# tuple packing and unpacking
+# a,b,c,d = 10,20,30,40
+# t=a,b,c,d
+# print(t)
+
+# t=(10,20,30,40)
+# a,b,c,d =t
+# print(a,b,c,d)
+
+# tuple comprehension
+# t = (x**2 for x in range(1,6))
+# print(type(t)) # here we are not getting tuple objects and we are getting generator object
+# for x in t:
+#     print(x)
+
+
+
+
+
+
